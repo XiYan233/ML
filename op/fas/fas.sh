0d4800a47fb438e875835c18579c43b33d685937
Demontisa <49113774+Demontisa@users.noreply.github.com>
2022-03-01 22:57:16 +0800

    更新

--- op/fas/fas.sh
+++ op/fas/fas.sh
@@ -164,14 +164,14 @@ systemctl enable iptables.service >/dev/null 2>&1
 function osuu02() {
 #--force --nodeps
 #安装epel仓库
+rpm -Va --nofiles --nodigest
 yum -y install epel-release
 #yum -y install openssl openssl-libs openssl-devel lzo lzo-devel pam pam-devel automake pkgconfig  gawk tar zip unzip  net-tools psmisc gcc httpd libxml2 libxml2-devel  bzip2 bzip2-devel libcurl libcurl-devel libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel gmp gmp-devel libmcrypt libmcrypt-devel readline readline-devel libxslt libxslt-devel dnsmasq iptables iptables-services
 #安装筑梦官方所需环境
-yum -y install telnet avahi openssl openssl-libs openssl-devel lzo lzo-devel pam pam-devel automake pkgconfig gawk tar zip unzip net-tools psmisc gcc pkcs11-helper mariadb mariadb-server httpd libxml2 libxml2-devel bzip2 bzip2-devel libcurl libcurl-devel libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel gmp gmp-devel libmcrypt libmcrypt-devel readline readline-devel libxslt libxslt-devel dnsmasq jre-1.7.0-openjdk
+yum -y install telnet avahi openssl openssl-libs openssl-devel lzo lzo-devel pam pam-devel automake pkgconfig gawk tar zip unzip net-tools psmisc gcc pkcs11-helper mariadb mariadb-server httpd libxml2 libxml2-devel bzip2 bzip2-devel libcurl libcurl-devel libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel gmp gmp-devel libmcrypt libmcrypt-devel readline readline-devel libxslt libxslt-devel dnsmasq jre-1.7.0-openjdk --skip-broken
 #安装PHP环境（这里使用的是第三方PHP安装源，并非筑梦官方安装源，此项并不影响正常安装使用使用）
 #安装PHP7.0，暂时不考虑PHP7.1   反正够用了，如您业务需要，请自行卸载PHP7.0版本安装PHP7.1版本即可
 rpm -Uvh ${http}${host}webtatic-release.rpm
-rpm -Va --nofiles --nodigest
 yum install php70w php70w-fpm php70w-bcmath php70w-cli php70w-common php70w-dba php70w-devel php70w-embedded php70w-enchant php70w-gd php70w-imap php70w-ldap php70w-mbstring php70w-mcrypt php70w-mysqlnd php70w-odbc php70w-opcache php70w-pdo php70w-pdo_dblib php70w-pear.noarch php70w-pecl-apcu php70w-pecl-apcu-devel php70w-pecl-imagick php70w-pecl-imagick-devel php70w-pecl-mongodb php70w-pecl-redis php70w-pecl-xdebug php70w-pgsql php70w-xml php70w-xmlrpc php70w-intl php70w-mcrypt --nogpgcheck php-fedora-autoloader php-php-gettext php-tcpdf php-tcpdf-dejavu-sans-fonts php70w-tidy -y --skip-broken
 #安装openvpn.rpm所需环境！
 rpm -Uvh ${http}${host}liblz4-1.8.1.2-alt1.x86_64.rpm
